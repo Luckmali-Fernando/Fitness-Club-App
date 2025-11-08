73ba393b8a861806d9aa579db59c097532b2dc8e
Luckmali-Fernando <luckyfernando92@gmail.com>
2025-11-08 10:10:35 +0530

    Codes

---
+++ lib/colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+  static const Color brown=Color(0x006c0b0b);
+
+  static const Color red=Color(0x00b01111);
+  static const Color lightred=Color(0x00e92222);
+  static const Color darkpink=Color(0x00f06666);
+  static const Color lightpink=Color(0x00f7aaaa);
+
+  static const Color white = Color(0x00ffffff);
+  
+}
---
+++ lib/text_styles.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'colors.dart';
+import 'fonts.dart';
+
+class AppTextStyles{
+  static const TextStyle mainHeading= TextStyle(
+    fontFamily: AppFonts.primaryFont,
+    fontSize: 32,
+    fontWeight:FontWeight.w400,
+    color: AppColors.white,
+  );
+
+}
